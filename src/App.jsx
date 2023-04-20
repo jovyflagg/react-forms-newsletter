972c6b695d93ca8464476d86bd6d82a92ddbcc68
Jovy Cortez <jovycortez11@gmail.com>
2023-04-20 15:42:07 -0400

    Navbar and a darmode toggle...pending styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Form from "./components/Form"
+import Navbar from "./components/Navbar";
 import "./style.css"
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Navbar />
         <Form />
       </header>
     </div>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+//import logo from "../images/logo-lightmode.png"
+
+export default function Navbar(props) {
+
+    return (
+         <nav 
+            className={props.darkMode ? "dark": ""}
+        >
+           
+            <h3 className="nav--logo_text">VIVA LA JOVY</h3>
+            
+            <div 
+                className="toggler" 
+            >
+                <p className="toggler--light">Light</p>
+                <div 
+                    className="toggler--slider"
+                    onClick={props.toggleDarkMode}
+                >
+
+                    <div className="toggler--slider--circle"></div>
+                </div>
+                <p className="toggler--dark">Dark</p>
+            </div>
+        </nav>
+    )
+}
